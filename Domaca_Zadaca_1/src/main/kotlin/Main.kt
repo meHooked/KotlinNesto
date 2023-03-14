3e53cb1b5bb1c0b66a88a8b56bb83f43f98e199f
renatodean1995 <renato.dean@predavaci.algebra.hr>
2023-03-14 20:38:25 +0100

    rekurzivne funkcije i lambda funkcije primjeri

--- Domaca_Zadaca_1/src/main/kotlin/Main.kt
+++ Domaca_Zadaca_1/src/main/kotlin/Main.kt
@@ -26,13 +26,21 @@ fun main(args: Array<String>) {
     /*
     Napravite program koji će tražiti unošenje prirodnog broja n<10. Program treba izračunati n! (faktorijel)
     Faktorijel se racuna kao 1 * 2 * 3 ... * n
+    PO definiciji 0! = 1 i 1! = 1
      */
+    val n = 5
+    println(factorial(10))
+    println(factorial2(10))
+    println(factorial3(10))
+    println(factorial4(10))
 
     //Zadatak 6:
     /*
     Napravite program koji će,koristeći se petljom for, izračunati i ispisati rezultat zbroja:
-     s=1+2^2+3^3+...+n^n (n < 10)
+     s=1^1+2^2+3^3+...+n^n (n < 10)
      */
+    println(sumRecursion(4)) // 256 + 27 + 4 + 1 = 256 + 32 = 288 OK
+    println(sumRecursion2(4))
 
     //Zadatak 7:
     /*
@@ -95,4 +103,50 @@ fun main(args: Array<String>) {
      */
 
 
+}
+
+fun factorial(n: Int): Int {
+    var product = 1
+    for(i in 1..n){
+        product = product * i //ili product *= 1
+    }
+    return product
+}
+
+fun factorial2(n: Int): Int {
+    var product = 1
+    var i = n
+    while(i > 1){
+        product *= i
+        i--
+    }
+    return product
+}
+
+//Faktorijel se definira kao
+/*
+f(x) = 1 , za x = 0 ili 1
+f(x) = x * f(x-1), za x > 1
+ */
+fun factorial3(n : Int): Int {
+    return when(n){
+        0,1 -> 1
+        else -> n * factorial3(n - 1)
+    }
+}
+
+fun factorial4(n: Int): Int{
+    return Array(n){it + 1}.reduce{acc, i -> acc * i}
+}
+
+fun sumRecursion(n: Int): Int {
+    return when(n){
+        0 -> 0
+        1 -> 1
+        else -> Math.pow(n * 1.0, n * 1.0).toInt() + sumRecursion(n - 1)
+    }
+}
+
+fun sumRecursion2(n: Int): Int {
+    return Array(n){it + 1}.reduce{acc, i -> acc + Math.pow(i * 1.0, i * 1.0).toInt()}
 }
