bbe919b26bab2fe6ddadc21524f84a297c953ef2
renatodean1995 <renato.dean@predavaci.algebra.hr>
2023-03-27 20:00:52 +0200

    dz1

--- Domaca_Zadaca_1/src/main/kotlin/Main.kt
+++ Domaca_Zadaca_1/src/main/kotlin/Main.kt
@@ -1,16 +1,5 @@
 fun main(args: Array<String>) {
-    //Zadatak 8:
-    /*
-    Napravite program koji će korisniku omogućiti učitavanje brojeva sve dok se ne unese -1.
-    Program treba ispisati na zaslon koliko je medu unesenim brojevima onih koji su djeljivi s 5.
-     */
 
-    //Zadatak 9:
-    /*
-    Napravite program koji omogućuje unošenje cijelih brojeva sve dok se ne unese 0.
-    Program treba ispisati koliko od unesenih brojeva ima barem jednu parnu znamenku.
-    (Testni primjer: 3 17 21 87 16 24 18 37 0 , Rjesenje: 5)
-     */
 }
 
 
